672989b94e71f0ac89f689f0bd3d7c58f3352f81
yiny <174821300@qq.com>
2021-09-04 23:04:42 +0800

    20210904-230442

---
+++ 06_src_scoped/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <School></School>
+      <hr>
+      <Student></Student>
+  </div>
+</template>
+
+<script>
+// 引入School组件
+import School  from './components/School'
+import Student from './components/Student'
+
+export default {
+    name: 'App',
+    components: {
+        School,
+        Student
+    }
+}
+</script>
---
+++ src/components/MyList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <ul>
+      <MyItem v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj"/>
+    </ul>    
+  </div>
+</template>
+
+<script>
+import MyItem from "./MyItem";
+
+export default {
+  name: "MyList",
+  components: {
+    MyItem
+  },
+  data() {
+    return {
+      todos: [
+        {id: '0001', title: '吃饭', done: true},
+        {id: '0002', title: '喝酒', done: false},
+        {id: '0003', title: '开车', done: true}
+      ]
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-main{
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty{
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
