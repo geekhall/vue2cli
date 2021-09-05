6472782b5377ce495d8f618483001be51121d48c
geekhall <174821300@qq.com>
2021-09-05 23:27:54 +0800

    20210905-232754

---
+++ src/components/TransactionTest.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <!-- <transition name="hello" :appear="true"> -->
+    <transition name="hello" appear>
+      <h1 v-show="isShow" class="go">你好啊</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TransactionTest",
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: cyan;
+}
+
+.hello-enter-active {
+  animation: geekhall 0.3s;
+}
+.hello-leave-active {
+  animation: geekhall 0.3s reverse;
+}
+@keyframes geekhall {
+  from {
+    transform: translateX(-100%);
+  }
+  to {
+    transform: translateX(0px);
+  }
+}
+</style>
