c73934e9d8be3312293f74bed589fd7a5031aab8
yiny <174821300@qq.com>
2021-09-05 19:42:33 +0800

    20210905-194233

---
+++ 09_src_组件自定义事件/components/School.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="school">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="sendSchoolName">把学校名字给App</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "极客堂",
+      address: "北京",
+    }
+  },
+  props: ['getSchoolName'],
+  methods: {
+    sendSchoolName(){
+      console.log(this.name)
+      this.getSchoolName(this.name) // 这里不要忘了this
+    }
+  }
+}
+</script>
+
+<style scoped>
+.school{
+  background-color: aqua;
+  margin: .5rem;
+  padding: .5rem;
+  
+}
+</style>
